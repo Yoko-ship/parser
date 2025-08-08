00cb040eb46b293ffca43e87b3c1fe1705f07955
User <yuko.brandon13@gmail.com>
2025-08-08 19:13:17 +0500

    added option parsing uzum

--- interface.py
+++ interface.py
@@ -6,23 +6,38 @@ class Interface:
         self.app.geometry("600x600")
         self.PADY = 5
         self.FONT = ("Times New Roman",20)
+        self.app.protocol("WM_DELETE_WINDOW",self.__on_close)
+        self.is_closed = False
         self.labels()
 
-    def __confirm_button(self):
-        self.__url = self.url_entry.get()
-        self.__product = self.product_entry.get()
+
+    def __on_close(self):
+        self.is_closed = True
         self.app.destroy()
-    
+
+    def __confirm_button(self):
+        self.__product = self.product_entry.get()
+        self.__user_choice = self.option_menu_var.get()
+        if not self.__product or not self.__user_choice:
+            self.error_label.configure(fg_color="red",text="Пожалуста укажите товар!")
+            return
+        self.app.destroy()
+        
     def labels(self):
-        customtkinter.CTkLabel(self.app,text="Ссылка на сайт",font=self.FONT).pack(pady=self.PADY)
-        self.url_entry = customtkinter.CTkEntry(self.app,placeholder_text="Url",width=300,font=self.FONT)
-        self.url_entry.pack(pady=self.PADY)
+        customtkinter.CTkLabel(self.app,text="Выберите интернет магазин",font=self.FONT).pack(pady=self.PADY)
+        self.option_menu_var = customtkinter.StringVar(value="OLX")
+        self.option_menu = customtkinter.CTkOptionMenu(self.app,values=["OLX","UZUM","Яндекс маркет","Озон"],variable=self.option_menu_var,width=300,font=self.FONT)
+        self.option_menu.pack(pady=self.PADY)
         customtkinter.CTkLabel(self.app,text="Что вы хотите найти?",font=self.FONT).pack(pady=self.PADY)
         self.product_entry = customtkinter.CTkEntry(self.app,width=300,font=self.FONT,placeholder_text="Поиск")
         self.product_entry.pack(pady=self.PADY)
         self.confirm = customtkinter.CTkButton(self.app,width=300,text="Подтвердить",font=self.FONT,command=self.__confirm_button).pack(pady=self.PADY)
+        self.error_label = customtkinter.CTkLabel(self.app,text="",font=self.FONT)
+        self.error_label.pack(pady=self.PADY)
         self.app.mainloop()
     
     def get_informations(self):
-        return (self.__url,self.__product)
-    
+        if not self.is_closed:
+          return (self.__product,self.__user_choice,self.is_closed)
+        else:
+            return (self.is_closed)
--- parsing.py
+++ parsing.py
@@ -5,51 +5,104 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from interface import Interface
-
+import time
 class Parsing:
     def __init__(self,url,search_element):
         self.url = url
         self.search_element = search_element
 
-    def parse_sites(self):
-        self.options = Options()
-        self.options.add_argument("--headless=new")
-        self.driver = webdriver.Chrome(self.options)
+    # def parse_olx(self):
+    #     self.options = Options()
+    #     self.options.add_argument("--headless=new")
+    #     self.driver = webdriver.Chrome(self.options)
+    #     self.driver.get(self.url)
+    #     self.input = WebDriverWait(self.driver,10).until(
+    #         EC.presence_of_element_located((By.ID,"search"))
+    #     )
+    #     self.input.clear()
+    #     self.input.send_keys(self.search_element)
+    #     self.input.send_keys(Keys.RETURN)
+    #     self.span = WebDriverWait(self.driver,10).until(
+    #         EC.presence_of_element_located((By.CSS_SELECTOR,".css-1r6clzs span"))
+    #     )
+
+    #     self.container = WebDriverWait(self.driver,10).until(
+    #         EC.presence_of_all_elements_located((By.CLASS_NAME,"css-1g5933j"))
+    #     )
+    #     self.title = []
+    #     self.href = []
+    #     self.price = []
+    #     self.condition = []
+    #     self.additional_information = []
+    #     for cont in self.container:
+    #         self.title.append(cont.find_element(By.CLASS_NAME,"css-1g61gc2").text)
+    #         self.href.append(cont.find_element(By.CLASS_NAME,"css-1tqlkj0").get_attribute("href"))
+    #         self.price.append(cont.find_element(By.CLASS_NAME,"css-uj7mm0").text)
+    #         self.condition.append(cont.find_element(By.CSS_SELECTOR,".css-iudov9 span").text)
+    #         self.additional_information.append(cont.find_element(By.CLASS_NAME,"css-vbz67q").text)
+
+    def get_data(self):
+        with open("Объявления.txt","w",encoding="UTF-8") as file:
+            file.write(f"{self.span.text} \n")
+            for t,p,h,c in zip(self.title,self.price,self.href,self.condition):
+                file.write(f"{t} || {p} || {h} || {c}  \n")
+            
+            print("Товары успешно добавлены в Объявления.txt")
+
+
+    def parse_web(self,input,span,container,title,href,price,condition):
+        # self.options = Options()
+        # self.options.add_argument("--headless=new")
+        self.driver = webdriver.Chrome()
         self.driver.get(self.url)
         self.input = WebDriverWait(self.driver,10).until(
-            EC.presence_of_element_located((By.ID,"search"))
+            EC.presence_of_element_located((By.CSS_SELECTOR,input))
         )
         self.input.clear()
         self.input.send_keys(self.search_element)
         self.input.send_keys(Keys.RETURN)
         self.span = WebDriverWait(self.driver,10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,".css-1r6clzs span"))
+            EC.presence_of_element_located((By.CSS_SELECTOR,span))
         )
 
+        # for i in range(1,4):
+        #     try:
+        #         page_link = self.driver.find_element(By.LINK_TEXT,str(i))
+        #         page_link.click()
         self.container = WebDriverWait(self.driver,10).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME,"css-1g5933j"))
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR,container))
         )
         self.title = []
         self.href = []
         self.price = []
         self.condition = []
-        self.additional_information = []
         for cont in self.container:
-            self.title.append(cont.find_element(By.CLASS_NAME,"css-1g61gc2").text)
-            self.href.append(cont.find_element(By.CLASS_NAME,"css-1tqlkj0").get_attribute("href"))
-            self.price.append(cont.find_element(By.CLASS_NAME,"css-uj7mm0").text)
-            self.condition.append(cont.find_element(By.CSS_SELECTOR,".css-iudov9 span").text)
-            self.additional_information.append(cont.find_element(By.CLASS_NAME,"css-vbz67q").text)
-
-    def get_data(self):
-        with open("Объявления.txt","w",encoding="UTF-8") as file:
-            file.write(f"{self.span.text} \n")
-            for t,p,h,c,a in zip(self.title,self.price,self.href,self.condition,self.additional_information):
-                file.write(f"{t} || {p} || {h} || {c} || {a} \n")
+            try:
+                self.title.append(cont.find_element(By.CSS_SELECTOR,title).text)
+                self.href.append(cont.find_element(By.CSS_SELECTOR,href).get_attribute("href"))
+                self.price.append(cont.find_element(By.CSS_SELECTOR,price).text)
+                self.condition.append(cont.find_element(By.CSS_SELECTOR,condition).text)
+            except Exception:
+                print("Skip")
+                continue
 
 interface = Interface()
 data = interface.get_informations()
-parse = Parsing(data[0],data[1])
-parse.parse_sites()
-parse.get_data()
+
+if isinstance(data,tuple):
+    product = data[0]
+    internet_magazin = data[1]
+    is_closed = data[2]
+    match internet_magazin:
+        case "OLX":
+            parse = Parsing("https://www.olx.uz/",product)
+            parse.parse_web("#search",".css-1r6clzs span",".css-1g5933j",".css-1g61gc2",".css-1tqlkj0",".css-uj7mm0",".css-iudov9 span")
+            parse.get_data()
+        
+        case "UZUM":
+            parse = Parsing("https://uzum.uz/ru",product)
+            parse.parse_web(".input-line input",".title h1 span","#category-products > div",".product-card__title","[data-test-id='product-card--default']",".currency",".reviews")
+            parse.get_data()
+
+
 
