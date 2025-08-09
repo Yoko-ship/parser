fe9b156120c5347a0402d992be76a262c6cadb57
User <yuko.brandon13@gmail.com>
2025-08-09 11:19:53 +0500

    yandex

--- parsing.py
+++ parsing.py
@@ -12,39 +12,9 @@ class Parsing:
         self.url = url
         self.search_element = search_element
 
-    # def parse_olx(self):
-    #     self.options = Options()
-    #     self.options.add_argument("--headless=new")
-    #     self.driver = webdriver.Chrome(self.options)
-    #     self.driver.get(self.url)
-    #     self.input = WebDriverWait(self.driver,10).until(
-    #         EC.presence_of_element_located((By.ID,"search"))
-    #     )
-    #     self.input.clear()
-    #     self.input.send_keys(self.search_element)
-    #     self.input.send_keys(Keys.RETURN)
-    #     self.span = WebDriverWait(self.driver,10).until(
-    #         EC.presence_of_element_located((By.CSS_SELECTOR,".css-1r6clzs span"))
-    #     )
-
-    #     self.container = WebDriverWait(self.driver,10).until(
-    #         EC.presence_of_all_elements_located((By.CLASS_NAME,"css-1g5933j"))
-    #     )
-    #     self.title = []
-    #     self.href = []
-    #     self.price = []
-    #     self.condition = []
-    #     self.additional_information = []
-    #     for cont in self.container:
-    #         self.title.append(cont.find_element(By.CLASS_NAME,"css-1g61gc2").text)
-    #         self.href.append(cont.find_element(By.CLASS_NAME,"css-1tqlkj0").get_attribute("href"))
-    #         self.price.append(cont.find_element(By.CLASS_NAME,"css-uj7mm0").text)
-    #         self.condition.append(cont.find_element(By.CSS_SELECTOR,".css-iudov9 span").text)
-    #         self.additional_information.append(cont.find_element(By.CLASS_NAME,"css-vbz67q").text)
-
     def get_data(self):
         with open("Объявления.txt","w",encoding="UTF-8") as file:
-            file.write(f"{self.span.text} \n")
+            # file.write(f"{self.span.text} \n")
             for t,p,h,c in zip(self.title,self.price,self.href,self.condition):
                 file.write(f"{t} || {p} || {h} || {c}  \n")
             
@@ -62,9 +32,9 @@ class Parsing:
         self.input.clear()
         self.input.send_keys(self.search_element)
         self.input.send_keys(Keys.RETURN)
-        self.span = WebDriverWait(self.driver,10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR,span))
-        )
+        # self.span = WebDriverWait(self.driver,10).until(
+        #     EC.presence_of_element_located((By.CSS_SELECTOR,span))
+        # )
 
         self.title = []
         self.href = []
@@ -102,5 +72,10 @@ if isinstance(data,tuple):
             parse.parse_web(".input-line input",".title h1 span","#category-products > div",".product-card__title","[data-test-id='product-card--default']",".currency",".reviews")
             parse.get_data()
 
+
+        case "Яндекс маркет":
+            parse = Parsing("https://market.yandex.uz/",product)
+            parse.parse_web("#header-search","","[data-auto='SerpGrid'] > div","[data-auto='snippet-title']",".EQlfk","[data-auto='snippet-price-current']","[data-baobab-name='rating'] > *:first-child")
+            parse.get_data()
 
 
