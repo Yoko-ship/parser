cac14656977a6d178713ace2ef566142a3ed1dc9
User <yuko.brandon13@gmail.com>
2025-08-09 18:19:15 +0500

    featured scrolling

--- parsing.py
+++ parsing.py
@@ -6,7 +6,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 from interface import Interface
+from selenium_stealth import stealth
 import time
+
+OLX_URL = "https://www.olx.uz/"
+UZUM_URL = "https://uzum.uz/ru" 
+YANDEX_URL = "https://market.yandex.uz/"
+OZON_URL = "https://uz.ozon.com/"
+
 class Parsing:
     def __init__(self,url,search_element):
         self.url = url
@@ -14,17 +21,21 @@ class Parsing:
 
     def get_data(self):
         with open("Объявления.txt","w",encoding="UTF-8") as file:
-            # file.write(f"{self.span.text} \n")
             for t,p,h,c in zip(self.title,self.price,self.href,self.condition):
                 file.write(f"{t} || {p} || {h} || {c}  \n")
             
             print("Товары успешно добавлены в Объявления.txt")
 
 
-    def parse_web(self,input,span,container,title,href,price,condition):
+    def parse_web(self,input,container,title,href,price,condition):
         self.options = Options()
         self.options.add_argument("window-size=1920,1080")
         self.driver = webdriver.Chrome(options=self.options)
+        stealth(self.driver,languages=['en-US',"en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine")
         self.driver.get(self.url)
         self.input = WebDriverWait(self.driver,10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR,input))
@@ -32,14 +43,15 @@ class Parsing:
         self.input.clear()
         self.input.send_keys(self.search_element)
         self.input.send_keys(Keys.RETURN)
-        # self.span = WebDriverWait(self.driver,10).until(
-        #     EC.presence_of_element_located((By.CSS_SELECTOR,span))
-        # )
+
 
         self.title = []
         self.href = []
         self.price = []
         self.condition = []
+        
+        if self.url == OZON_URL or self.url == YANDEX_URL:
+            self.scroll_page()
         
         self.container = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, container))
@@ -48,11 +60,21 @@ class Parsing:
         for cont in self.container:
             try:
                 self.title.append(cont.find_element(By.CSS_SELECTOR, title).text)
+
                 self.href.append(cont.find_element(By.CSS_SELECTOR, href).get_attribute("href"))
                 self.price.append(cont.find_element(By.CSS_SELECTOR, price).text)
                 self.condition.append(cont.find_element(By.CSS_SELECTOR, condition).text)
             except Exception:
                 continue
+
+        
+    def scroll_page(self):
+        for _ in range(70):
+            self.driver.execute_script("window.scrollBy(0,100)")
+            time.sleep(0.1)
+            WebDriverWait(self.driver,5).until(
+                lambda d: d.execute_script("return document.readyState") == 'complete'
+            )
 
 interface = Interface()
 data = interface.get_informations()
@@ -63,19 +85,25 @@ if isinstance(data,tuple):
     is_closed = data[2]
     match internet_magazin:
         case "OLX":
-            parse = Parsing("https://www.olx.uz/",product)
-            parse.parse_web("#search",".css-1r6clzs span",".css-1g5933j",".css-1g61gc2",".css-1tqlkj0",".css-uj7mm0",".css-iudov9 span")
+            parse = Parsing(OLX_URL,product)
+            parse.parse_web("#search",".css-1g5933j",".css-1g61gc2",".css-1tqlkj0",".css-uj7mm0",".css-iudov9 span")
             parse.get_data()
         
         case "UZUM":
-            parse = Parsing("https://uzum.uz/ru",product)
-            parse.parse_web(".input-line input",".title h1 span","#category-products > div",".product-card__title","[data-test-id='product-card--default']",".currency",".reviews")
+            parse = Parsing(UZUM_URL,product)
+            parse.parse_web(".input-line input","#category-products > div",".product-card__title","[data-test-id='product-card--default']",".currency",".reviews")
             parse.get_data()
 
 
         case "Яндекс маркет":
-            parse = Parsing("https://market.yandex.uz/",product)
-            parse.parse_web("#header-search","","[data-auto='SerpGrid'] > div","[data-auto='snippet-title']",".EQlfk","[data-auto='snippet-price-current']","[data-baobab-name='rating'] > *:first-child")
+            parse = Parsing(YANDEX_URL,product)
+            parse.parse_web("#header-search","[data-auto='SerpList'] > div, [data-auto='SerpGrid'] > div","[data-auto='snippet-title']",".EQlfk","[data-auto='snippet-price-current']","[data-baobab-name='rating'] > *:first-child")
+            parse.get_data()
+        
+        case "Озон":
+            parse = Parsing(OZON_URL,product)
+            parse.parse_web(".tr8_31",".i7u_24 > div",".bq02_5_0-a span",".q4b1_3_0-a",".c35_3_2-a0 > *:first-child",".p6b2_5_0-a4 > *:nth-child(2)")
             parse.get_data()
 
+            
 
